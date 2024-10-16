09ee6ada1ea04f6aef656c5967797382c27d33cc
dmi.a.petrov <dmi.a.petrov@tinkoff.ru>
2024-10-16 23:42:18 +0300

    add test directory

---
+++ src/test/java/random_walk/services/InitialTest.java
@@ -0,0 +1,10 @@
+package random_walk.services;
+
+import org.junit.jupiter.api.Test;
+
+public class InitialTest {
+    @Test
+    public void test() {
+        System.out.println("hello");
+    }
+}
