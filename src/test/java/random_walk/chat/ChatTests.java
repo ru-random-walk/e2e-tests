f83f372eb58303ee77b492fffc129b918c09686b
dmiAPetrov <dmiapetrov@mail.ru>
2025-04-06 12:05:35 +0300

    RANDOMWALK-166 | add base chat test and fix pipeline

--- src/test/java/random_walk/chat/ChatTests.java
+++ src/test/java/random_walk/chat/ChatTests.java
@@ -1,11 +1,19 @@
 package random_walk.chat;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import random_walk.BaseTest;
 import random_walk.automation.api.chat.services.ChatApi;
 
-public class ChatTests extends BaseTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ChatTests extends ChatTest {
 
     @Autowired
     private ChatApi chatApi;
+
+    @Test
+    void f() {
+        var a = 1;
+        assertEquals(1, a);
+    }
 }
