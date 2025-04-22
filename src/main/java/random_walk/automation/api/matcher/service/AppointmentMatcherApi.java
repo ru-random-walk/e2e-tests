967c57afed8776e659d9df6ea99ee2923940f7c4
dmiAPetrov <dmiapetrov@mail.ru>
2025-04-22 21:38:03 +0300

    RANDOMWALK-126 | create some matcher tests

---
+++ src/main/java/random_walk/automation/api/matcher/service/AppointmentMatcherApi.java
@@ -0,0 +1,40 @@
+package random_walk.automation.api.matcher.service;
+
+import io.qameta.allure.Step;
+import org.springframework.stereotype.Service;
+import random_walk.automation.api.matcher.MatcherConfigurationProperties;
+import random_walk.automation.config.TestTokenConfig;
+import random_walk.automation.config.filter.BearerAuthToken;
+import ru.random_walk.swagger.matcher_service.api.AppointmentControllerApi;
+import ru.random_walk.swagger.matcher_service.invoker.ApiClient;
+import ru.random_walk.swagger.matcher_service.model.AppointmentDetailsDto;
+
+import static random_walk.automation.config.SwaggerConfig.getSupplierWithUri;
+
+@Service
+public class AppointmentMatcherApi {
+
+    private final AppointmentControllerApi api;
+
+    private final String token;
+
+    public AppointmentMatcherApi(MatcherConfigurationProperties properties, TestTokenConfig testTokenConfig) {
+        this.api = ApiClient
+                .api(ApiClient.Config.apiConfig().reqSpecSupplier(getSupplierWithUri(properties.httpEndpoint().host())))
+                .appointmentController();
+        this.token = testTokenConfig.getToken();
+    }
+
+    @Step("[MATCHER_SERVICE: /appointment/{appointmentId}] Получение деталей встречи по id")
+    public AppointmentDetailsDto getInfoByAppointment(String appointmentId) {
+        return api.getAppointment()
+                .reqSpec(r -> r.addFilter(new BearerAuthToken(token)))
+                .appointmentIdPath(appointmentId)
+                .execute(r -> r.as(AppointmentDetailsDto.class));
+    }
+
+    @Step("[MATCHER_SERVICE: /appointment/{appointmentId}/cancel] Отмена назначенной встречи")
+    public void cancelAppointmentById(String appointmentId) {
+        api.cancelAppointment().reqSpec(r -> r.addFilter(new BearerAuthToken(token))).appointmentIdPath(appointmentId);
+    }
+}
