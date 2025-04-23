5959b1e234a701bd3df0429305921ef5c997fa18
dmiAPetrov <dmiapetrov@mail.ru>
2025-04-24 00:12:39 +0300

    RANDOMWALK-126 | checkout commit

--- src/test/java/random_walk/matcher/person_controller/PersonScheduleTest.java
+++ src/test/java/random_walk/matcher/person_controller/PersonScheduleTest.java
@@ -2,4 +2,6 @@ package random_walk.matcher.person_controller;
 
 import random_walk.matcher.MatcherTest;
 
-public class PersonScheduleTest extends MatcherTest {}
+public class PersonScheduleTest extends MatcherTest {
+
+}
