74e4884d15d370f33a1063c7b6aa24df86465dfb
dmiAPetrov <dmiapetrov@mail.ru>
2025-05-10 14:01:13 +0300

    RANDOMWALK-126 | remove testing test

---
+++ src/test/java/random_walk/matcher/appointment_controller/CancelAppointmentTest.java
@@ -0,0 +1,14 @@
+package random_walk.matcher.appointment_controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import random_walk.matcher.MatcherTest;
+
+public class CancelAppointmentTest extends MatcherTest {
+
+    @Test
+    @DisplayName("Отмена прогулки пользователем")
+    void checkCancelAppointment() {
+
+    }
+}
