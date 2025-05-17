c99e1093bdcf94f217090fe7d1c3eef2c11a3921
dmiAPetrov <dmiapetrov@mail.ru>
2025-05-18 01:05:48 +0300

    RANDOMWALK-113 | fix

--- src/test/java/random_walk/auth/Test.java
+++ src/test/java/random_walk/auth/Test.java
@@ -1,8 +1,11 @@
 package random_walk.auth;
 
+import org.junit.jupiter.api.Disabled;
+
 public class Test extends AuthTest {
 
     @org.junit.jupiter.api.Test
+    @Disabled
     void f() {
         var token = authServiceApi.getAuthTokens("").getAccessToken();
         authServiceApi.logout(token);
